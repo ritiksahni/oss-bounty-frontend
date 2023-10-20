8150534e81681d03fc8419fe0cf625a9fd3ef860
Ritik Sahni <ritiksahni0203@gmail.com>
2023-10-20 14:31:00 +0530

    Add ellipsis to issueDescription in BountyCard.jsx

--- src/components/BountyCard.jsx
+++ src/components/BountyCard.jsx
@@ -4,21 +4,21 @@ import { Link } from 'react-router-dom';
 
 const BountyCard = ({ bountyData }) => {
   return (
-    <Card size="3" style={{ maxWidth: 320 }}>
+    <Card size="3" className={styles.cardComponent}>
       <Inset clip="padding-box" side="top" pb="current">
         <Link to={bountyData.repoLink} target="_blank">
           <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt={bountyData.repoLink} className={styles.cardImage} />
         </Link>
       </Inset>
 
-    <Text as="p" size="3">
-      <RadixLink asChild><Link to={bountyData.repoLink}>{bountyData.repoLink}</Link></RadixLink>
-      <br />
-      <Strong>Amount</Strong>: {bountyData.bounty_amount}
-      <br />
-      <Strong>Issue</Strong>: {bountyData.issueDescription}
-      <br />
-    </Text>
+      <Text as="p" size="3">
+        <RadixLink asChild><Link to={bountyData.repoLink}>{bountyData.repoLink}</Link></RadixLink>
+        <br />
+        <Strong>Amount</Strong>: {bountyData.bounty_amount}
+        <br />
+        <Strong>Issue</Strong>: <span className={styles.issueDescription}>{bountyData.issueDescription}</span>
+        <br />
+      </Text>
     </Card>
   );
 }
