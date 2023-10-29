ceb82dd79e5fe71db550bd4d30ecac73f125a5c6
Ritik Sahni <ritiksahni0203@gmail.com>
2023-10-29 15:18:40 +0530

    Add logout function

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,3 +1,7 @@
 export function login() {
     window.open(`${process.env.EXPRESS_SERVER_URL}/api/auth/login`, "_self");
 }
+
+export function logout() {
+    window.open(`${process.env.EXPRESS_SERVER_URL}/api/auth/logout`, "_self");
+}
