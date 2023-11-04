d47caac9215b7fd6097c2f8adafbfdfa2428b3c8
Ritik Sahni <ritiksahni0203@gmail.com>
2023-11-04 22:23:49 +0530

    Add fetchClaims function in ListClaims.jsx

--- src/pages/ListClaims.jsx
+++ src/pages/ListClaims.jsx
@@ -1,10 +1,33 @@
 import { useParams } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query";
+import axios from 'axios';
 
 function ListClaims() {
     const { id } = useParams();
+
+    async function fetchClaims() {
+        const res = await axios.post(`${process.env.EXPRESS_SERVER_URL}/api/list-claims`, { "bounty_id": id });
+        return res.data;
+    }
+    const { isLoading, error, data } = useQuery({
+        queryKey: ['id'],
+        queryFn: fetchClaims,
+    });
+
     return(
         <div>
             <h2>This page will have all the claims for bounty {id}</h2>
+
+            {isLoading && <p>Loading...</p>}
+
+            {error && <p>Error: {error.status}</p>}
+
+            {Array.isArray(data) && data.map((claim) => (
+                <div key={claim.id}>
+                    <h3>{claim.title}</h3>
+                    <p>{claim.description}</p>
+                </div>
+            ))}
         </div>
     )
 }
